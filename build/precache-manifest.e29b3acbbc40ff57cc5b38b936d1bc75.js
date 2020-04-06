9b82da1a6543276c3526d486cbe361bb08ffb11e
ritusinha128 <risi1999@gmail.com>
2020-04-07 00:24:04 +0530

    first

---
+++ build/precache-manifest.e29b3acbbc40ff57cc5b38b936d1bc75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517ebc6ec9a12f89c4c8e0a71ca7c17d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfc13db0c642b147c0fd",
+    "url": "/static/css/main.480ba89e.chunk.css"
+  },
+  {
+    "revision": "1c70485e7cfd1e1a9bac",
+    "url": "/static/js/2.f76bf850.chunk.js"
+  },
+  {
+    "revision": "cfc13db0c642b147c0fd",
+    "url": "/static/js/main.1c3927d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
